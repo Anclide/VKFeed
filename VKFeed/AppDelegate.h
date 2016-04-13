eaf43beb1bad3283bd15dd75e11c0835ee01cf74
Victor Bogatyrev <victorbogatyrev@macbook-pro-victor.local>
2016-04-13 14:45:59 +0300

    Initial Commit

---
+++ VKFeed/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  VKFeed
+//
+//  Created by Victor Bogatyrev on 13.04.16.
+//  Copyright © 2016 Victor Bogatyrev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ VKFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKFeed
+//
+//  Created by Victor Bogatyrev on 13.04.16.
+//  Copyright © 2016 Victor Bogatyrev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
